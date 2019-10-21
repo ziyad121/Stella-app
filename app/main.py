67a582fda711712cf9f4b5d50abb43abba42957e
ziyad meftah <ziyadmeftah@gmail.com>
2019-10-21 10:52:34 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 container_dict = {"lucene": {"requests": 0},
-                  "solr": {"requests": 0}}
+                  "solr": {"requests": 0}, "elastic":{"requests":0}}
 
 
 #container_dict = {"elastic":{"requests":0}}
@@ -78,7 +78,7 @@ def ranking(query):
     print("****************")
 
     exec_res = container.exec_run(cmd)
-    #exec_res['name']=container
+    exec_res.append({'name':container})
     print(exec_res)
     exec_dict = json.loads(exec_res.output.decode("utf-8"))
     return jsonify(exec_dict)
